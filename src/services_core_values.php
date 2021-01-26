d95a611f090846a52eae6cf4a80168537b21e638
Durrell Gemuh <durrellgemuh07@gmail.com>
2021-01-26 13:32:32 +0100

    fini

--- src/services_core_values.php
+++ src/services_core_values.php
@@ -10,63 +10,63 @@
         <div class="row">
           <div class="col-lg-4 col-md-6" data-aos="fade-up">
             <div class="icon-box">
-              <div class="icon"><i class="icofont-bulb"></i></div>
+              <div class="icon"><i class="icofont-info"></i></div>
               <h4 class="title"><a href="">1</a></h4>
               <p class="description">End hunger and poverty, safe the environment.</p>
             </div>
           </div>
           <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="100">
             <div class="icon-box">
-              <div class="icon"><i class="icofont-chart-bar-graph"></i></div>
+              <div class="icon"><i class="icofont-info"></i></div>
               <h4 class="title"><a href="">2</a></h4>
               <p class="description">Birth control, male contraceptive.</p>
             </div>
           </div>
           <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="200">
             <div class="icon-box">
-              <div class="icon"><i class="icofont-earth"></i></div>
+              <div class="icon"><i class="icofont-info"></i></div>
               <h4 class="title"><a href="">3</a></h4>
               <p class="description">Increase education and capacity building.</p>
             </div>
           </div>
           <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="300">
             <div class="icon-box">
-              <div class="icon"><i class="icofont-image"></i></div>
+              <div class="icon"><i class="icofont-info"></i></div>
               <h4 class="title"><a href="">4</a></h4>
               <p class="description">Family planing and welfare.</p>
             </div>
           </div>
           <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="400">
             <div class="icon-box">
-              <div class="icon"><i class="icofont-settings"></i></div>
+              <div class="icon"><i class="icofont-info"></i></div>
               <h4 class="title"><a href="">5</a></h4>
               <p class="description">Domestic violence gender, base violence, nagging and provocation.</p>
             </div>
           </div>
           <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="500">
             <div class="icon-box">
-              <div class="icon"><i class="icofont-tasks-alt"></i></div>
+              <div class="icon"><i class="icofont-info"></i></div>
               <h4 class="title"><a href="">6</a></h4>
               <p class="description">Family comfort satisfaction and sustainability.</p>
             </div>
           </div>
           <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="500">
             <div class="icon-box">
-              <div class="icon"><i class="icofont-tasks-alt"></i></div>
+              <div class="icon"><i class="icofont-info"></i></div>
               <h4 class="title"><a href="">7</a></h4>
               <p class="description">Decreases HIV/AIDS and other transmissible diseases.</p>
             </div>
           </div>
           <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="500">
             <div class="icon-box">
-              <div class="icon"><i class="icofont-tasks-alt"></i></div>
+              <div class="icon"><i class="icofont-info"></i></div>
               <h4 class="title"><a href="">8</a></h4>
               <p class="description">Improve maternal health.</p>
             </div>
           </div>
           <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="500">
             <div class="icon-box">
-              <div class="icon"><i class="icofont-tasks-alt"></i></div>
+              <div class="icon"><i class="icofont-info"></i></div>
               <h4 class="title"><a href="">9</a></h4>
               <p class="description">Improve infant health and children's rights.</p>
             </div>
